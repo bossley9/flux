769e57436486fe05885ea73c14df5e636020f1a4
Sam Bossley <bossley.samuel@gmail.com>
2023-01-27 20:26:24 -0800

    feat: add tanstack query

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import {StatusBar} from 'expo-status-bar';
 import {StyleSheet, Text, View} from 'react-native';
 import {TestComponent} from './src/components/TestComponent'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <TestComponent />
-      <StatusBar style="auto" />
-    </View>
+    <QueryClientProvider client={queryClient}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <TestComponent />
+        <StatusBar style="auto" />
+      </View>
+    </QueryClientProvider>
   );
 }
 
--- src/components/TestComponent.tsx
+++ src/components/TestComponent.tsx
@@ -1,7 +1,17 @@
 import {Text} from 'react-native';
+import {useQuery} from '@tanstack/react-query'
 
 export function TestComponent() {
+  const {data, isLoading} = useQuery({
+    queryKey: ['star wars'],
+    queryFn: () =>
+      fetch('https://swapi.dev/api/planets/1/', {method: 'GET'}).then(res => res.json())
+  })
+
   return (
-    <Text>This is a test component!</Text>
+    <>
+      <Text>This is a test component!</Text>
+      {isLoading ? <Text>fetching data...</Text> : <Text>data is {JSON.stringify(data)}</Text>}
+    </>
   )
 }
