8281b7491499174402230e84c8de91599dd020b8
Sam Bossley <bossley.samuel@gmail.com>
2023-01-30 17:25:57 -0800

    feat: add tab icons

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,11 +18,11 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint'],
   rules: {
-    indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'never'],
     '@typescript-eslint/no-unused-vars': ['error'],
+    indent: ['error', 2, { SwitchCase: 1 }],
   },
   settings: {
     react: {
--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -3,22 +3,56 @@ import { ReadScreen } from '@/screens/ReadScreen'
 import { UnreadScreen } from '@/screens/UnreadScreen'
 import { HistoryScreen } from '@/screens/HistoryScreen'
 import { SettingsScreen } from '@/screens/SettingsScreen'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { tokens } from '@/styles'
 import { MainScreen as MainScreenType } from '@/navigation'
 import type { BottomTabNavigationOptions } from '@react-navigation/bottom-tabs'
+import type {
+  DefaultNavigatorOptions,
+  EventMapBase,
+} from '@react-navigation/core'
+import type { NavigationState } from '@react-navigation/routers'
 import type { MainTabParamList } from '@/navigation'
+
+type ScreenOptions = DefaultNavigatorOptions<
+  MainTabParamList,
+  NavigationState,
+  BottomTabNavigationOptions,
+  EventMapBase
+>['screenOptions']
 
 const Tab = createBottomTabNavigator<MainTabParamList>()
 
 export function MainScreen() {
-  const screenOptions: BottomTabNavigationOptions = {
+  const screenOptions: ScreenOptions = ({ route }) => ({
     headerShown: false,
     tabBarStyle: {
       backgroundColor: tokens.backgroundColor,
       elevation: 0,
     },
-  }
+    tabBarIcon: ({ color, size }) => {
+      let iconName: string
+      switch (route.name) {
+        case MainScreenType.Feeds:
+          iconName = 'rss-feed'
+          break
+        case MainScreenType.Read:
+          iconName = 'mail'
+          break
+        case MainScreenType.Unread:
+          iconName = 'mark-email-unread'
+          break
+        case MainScreenType.History:
+          iconName = 'history'
+          break
+        case MainScreenType.Settings:
+        default:
+          iconName = 'settings'
+      }
+      return <Icon name={iconName} size={size} color={color} />
+    },
+  })
   return (
     <Tab.Navigator
       initialRouteName={MainScreenType.Unread}
