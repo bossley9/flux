27020a8fc373fd15a360e46f41b835e5dfa53213
Sam Bossley <bossley.samuel@gmail.com>
2023-02-03 16:40:53 -0800

    feat: implement basic list functionality and uniquely identify child node keys

--- src/html/RenderHtml.tsx
+++ src/html/RenderHtml.tsx
@@ -5,6 +5,14 @@ import { linkStyles } from './Link'
 import { parse, Node, NodeType } from 'node-html-parser'
 import { tokens } from '@/tokens'
 
+function getKey(node: Node, index: number): string {
+  return node.range[0] + '-' + node.range[1] + '-' + index
+}
+
+function isWhitespaceNode(node: Node): boolean {
+  return node.nodeType === NodeType.TEXT_NODE && node.text.trim().length === 0
+}
+
 function getNodeAttribute(node: Node, attr: string): string | null {
   const src = node.toString()
   const tagMeta = src.substring(1, src.indexOf('>'))
@@ -35,16 +43,54 @@ function renderLinkNode(node: Node, index: number): ReactNode {
     }
   }
   return (
-    <Text key={index} onPress={handlePress} style={linkStyles}>
+    <Text key={getKey(node, index)} onPress={handlePress} style={linkStyles}>
       {node.childNodes.map(renderNode)}
     </Text>
   )
 }
 
+function renderListItemNode(node: Node, index: number): ReactNode {
+  const parent = node.parentNode
+  const parentType = parent.tagName.toLocaleLowerCase()
+  const key = getKey(node, index)
+
+  const listIndex =
+    parent.childNodes
+      // filter out whitespace values
+      .filter((child) => !isWhitespaceNode(child))
+      .findIndex((child) => child === node) + 1
+
+  switch (parentType) {
+    case 'ul':
+      return (
+        <P key={key} margin={tokens.space / 4}>
+          • {node.childNodes.map(renderNode)}
+        </P>
+      )
+
+    case 'ol':
+      return (
+        <P
+          key={key}
+          margin={tokens.space / 4}
+          style={{ backgroundColor: 'yellow' }}
+        >
+          {listIndex}. {node.childNodes.map(renderNode)}
+        </P>
+      )
+
+    default:
+      // sanity check - if all else fails, render a normal node
+      return <Text key={key}>{node.childNodes.map(renderNode)}</Text>
+  }
+}
+
 function renderElementNode(node: Node, index: number): ReactNode {
-  if (node.childNodes.length === 0) {
+  const children = node.childNodes
+  if (children.length === 0) {
     return null
   }
+  const key = getKey(node, index)
 
   const src = node.toString()
   // < is index 0, element starts at index 1
@@ -57,18 +103,27 @@ function renderElementNode(node: Node, index: number): ReactNode {
 
   switch (tagName) {
     case 'p':
-      return <P key={index}>{node.childNodes.map(renderNode)}</P>
+      return <P key={key}>{children.map(renderNode)}</P>
     case 'a':
       return renderLinkNode(node, index)
     case 'strong':
       return (
-        <P key={index} style={{ fontWeight: 'bold' }}>
-          {node.childNodes.map(renderNode)}
+        <P key={key} style={{ fontWeight: 'bold' }}>
+          {children.map(renderNode)}
         </P>
       )
+    case 'ul':
+    case 'ol':
+      return (
+        <View key={key} style={{ paddingLeft: tokens.space }}>
+          {children.map(renderNode)}
+        </View>
+      )
+    case 'li':
+      return renderListItemNode(node, index)
     default:
       return (
-        <P key={index} color={tokens.errorColor}>
+        <P key={key} color={tokens.errorColor}>
           unimplemented tag &lt;{tagName}&gt;
         </P>
       )
@@ -76,15 +131,14 @@ function renderElementNode(node: Node, index: number): ReactNode {
 }
 
 function renderTextNode(node: Node, index: number): ReactNode {
-  if (node.text.trim().length === 0) {
-    return null
-  }
   // sanity check - we shouldn't need a wrapper but sometimes
   // special elements such as iframes break the rendering
-  return <P key={index}>{node.text}</P>
+  return <Text key={getKey(node, index)}>{node.text}</Text>
 }
 
 function renderNode(node: Node, index: number): ReactNode {
+  if (isWhitespaceNode(node)) return null
+
   switch (node.nodeType) {
     case NodeType.ELEMENT_NODE:
       return renderElementNode(node, index)
